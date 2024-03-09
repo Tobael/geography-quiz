894d7e0878a0063b9a238cf4a130ebca3bb736f7
Michael Strobel <mstrobel97@gmail.com>
2024-03-09 22:30:03 +0100

    feat(FlagQuiz): simple working example

---
+++ src/components/FlagQuiz.vue
@@ -0,0 +1,61 @@
+<script>
+import '../../node_modules/flag-icons/css/flag-icons.min.css'
+import * as countries from '@/assets/countries.json';
+
+export default {
+  data() {
+    return {
+      current: this.randomCountry(),
+      score: 0,
+      input: '',
+    }
+  },
+
+  methods: {
+    randomCountry() {
+      const keys = Object.keys(countries)
+      const random = (keys.length * Math.random()) << 0
+      const countryCode = keys[random]
+      const countryNames = countries[countryCode]
+      return {
+        code: countryCode.toLowerCase(),
+        names: countryNames.map(c => c.toLowerCase()),
+      }
+    },
+
+    check() {
+      if (this.current.names.includes(this.input.toLowerCase())) {
+        this.score++
+        this.input = ''
+        this.current = this.randomCountry()
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Guess the Flag</h1>
+  <hr>
+  <div class="flag">
+    <v-icon :icon="`fi fi-${current.code}`"></v-icon>
+  </div>
+  <div class="input">
+    <v-text-field v-model="input" label="Country" autofocus variant="outlined" @keyup="check"></v-text-field>
+  </div>
+  <hr>
+  <div class="score">
+    {{ score }}
+  </div>
+</template>
+
+<style scoped>
+.flag .fi {
+  width: 100%;
+  height: 10rem;
+}
+
+.score {
+  width: 100%;
+}
+</style>
